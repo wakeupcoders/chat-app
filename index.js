8114b48cb630489c427d7cb6579f3e2222120818
Narayan Jha <wakeupcoders@gmail.com>
2025-10-23 12:59:29 +0530

    new changes

--- index.js
+++ index.js
@@ -12,33 +12,23 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-
-  // listen for the "join" event where the client sends the username
   socket.on('join', (username) => {
-    socket.username = username;               // store username on socket
-    // broadcast to all **others** that this user has joined
-    socket.broadcast.emit('chat message', `${username} has joined the chat`);
-    console.log(`${username} joined`);
+    socket.username = username;
+    socket.broadcast.emit('chat message', { system: true, text: `${username} joined the chat` });
   });
 
-  // listen for chat messages
   socket.on('chat message', (msg) => {
     const username = socket.username || 'Anonymous';
-    // send to all including sender
-    io.emit('chat message', `${username}: ${msg}`);
-    console.log(`${username}: ${msg}`);
+    io.emit('chat message', { username, text: msg });
   });
 
   socket.on('disconnect', () => {
-    const username = socket.username || 'Someone';
-    // broadcast to others
-    socket.broadcast.emit('chat message', `${username} has left the chat`);
-    console.log('user disconnected', socket.id, username);
+    const username = socket.username;
+    if (username) {
+      socket.broadcast.emit('chat message', { system: true, text: `${username} left the chat` });
+    }
   });
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
-});
+server.listen(PORT, () => console.log(`listening on *:${PORT}`));
