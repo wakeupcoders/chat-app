c734ca6138c44feec95d84a42d71e6c1e7e3278e
wakeupcoders <63137948+wakeupcoders@users.noreply.github.com>
2025-10-30 13:13:47 +0530

    Add test comment in index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+this is test
 app.use(express.static(__dirname + "/public"));
 
 const users = {}; // username -> socket.id
